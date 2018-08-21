7b30afa5cbb33e00856913ec70e377ae37015e7b
xieyunzi <xieyunzi@foxmail.com>
2018-08-21 18:25:58 +0800

    init

---
+++ Makefile
@@ -0,0 +1,19 @@
+CFLAGS = -Wall -g -I. -I./include -I./gnulib/lib
+BUILD_DIR = ./build
+
+all: directories build
+
+build-o:
+	make -C src
+
+build: build-o
+	gcc $(CFLAGS) -o $(BUILD_DIR)/cat  src/cat.o
+
+clean:
+	find . -name '*.[od]' -print0 | xargs -0 -r rm -v
+	rm -rf -v $(BUILD_DIR)/*
+
+directories:
+	mkdir -p build
+
+.PHONY: directories build clean all
---
+++ src/cat.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+
+# define ZERO_BYTE_TRANSFER_ERRNO 0
+
+size_t
+full_write (int fd, const void *buf, size_t count)
+{
+  size_t total = 0;
+  const char *ptr = (const char *) buf;
+
+  while (count > 0)
+  {
+    size_t n_rw = write(fd, ptr, count);
+    if (n_rw == (size_t) -1)
+      break;
+    if (n_rw == 0)
+    {
+      errno = ZERO_BYTE_TRANSFER_ERRNO;
+      break;
+    }
+    total += n_rw;
+    ptr += n_rw;
+    count -= n_rw;
+  }
+
+  return total;
+}
+
+static bool
+simple_cat (
+     /* Pointer to the buffer, used by reads and writes.  */
+     char *buf,
+
+     /* Number of characters preferably read or written by each read and write
+        call.  */
+     size_t bufsize)
+{
+  /* Actual number of characters read, and therefore written.  */
+  size_t n_read;
+
+  /* Loop until the end of the file.  */
+
+  while (true)
+  {
+    /* Read a block of input.  */
+    n_read = read (STDIN_FILENO, buf, bufsize);
+
+    if (n_read == -1)
+      printf("read error"), exit(EXIT_FAILURE);
+
+    /* End of this file?  */
+    if (n_read == 0)
+      return true;
+
+    /* Write this block out.  */
+    {
+      /* The following is ok, since we know that 0 < n_read.  */
+      size_t n = n_read;
+      if (full_write (STDOUT_FILENO, buf, n) != n)
+        printf("write error"), exit(EXIT_FAILURE);
+    }
+  }
+}
+
+int
+main () {
+  char *inbuf;
+  /* Name of input file.  May be "-".  */
+  static char const *infile;
+  infile = "-";
+  inbuf = malloc((size_t) 128 * 1024);
+  simple_cat(inbuf, 128 * 1024);
+}
